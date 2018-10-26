d07f0fd982d263e4672233777e202abacbe5e946
cathy.zhang@didata.com <cathy.zhang@dimensiondata.com>
2018-10-26 16:21:45 +1100

    update startup

--- WebAzure/Program.cs
+++ WebAzure/Program.cs
@@ -19,6 +19,7 @@ namespace WebAzure
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
